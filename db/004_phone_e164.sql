7ca685266bb1580a978264923dcb8fba82110205
suvankar-srtec <suvankar.srtec@gmail.com>
2026-09-23 18:29:33 +0530

    Make phone migration idempotent

--- db/004_phone_e164.sql
+++ db/004_phone_e164.sql
@@ -5,15 +5,34 @@ ALTER TABLE public.bbc_event_registrations
 ALTER TABLE public.bbc_event_registrations
   ALTER COLUMN phone TYPE VARCHAR(16);
 
-UPDATE public.bbc_event_registrations
-SET phone = CASE
-  WHEN phone LIKE '+%' THEN phone
-  ELSE COALESCE(NULLIF(phone_country_code, ''), '+91') || phone
-END;
+DO $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'bbc_event_registrations'
+      AND column_name = 'phone_country_code'
+  ) THEN
+    UPDATE public.bbc_event_registrations
+    SET phone = CASE
+      WHEN phone LIKE '+%' THEN phone
+      ELSE COALESCE(NULLIF(phone_country_code, ''), '+91') || phone
+    END;
+
+    ALTER TABLE public.bbc_event_registrations
+      DROP COLUMN phone_country_code;
+  ELSE
+    UPDATE public.bbc_event_registrations
+    SET phone = '+91' || phone
+    WHERE phone !~ '^\\+';
+  END IF;
+END
+$$;
+
+ALTER TABLE public.bbc_event_registrations
+  DROP CONSTRAINT IF EXISTS bbc_event_registrations_phone_check;
 
 ALTER TABLE public.bbc_event_registrations
   ADD CONSTRAINT bbc_event_registrations_phone_check
-  CHECK (phone ~ '^\+[1-9][0-9]{7,14}$');
-
-ALTER TABLE public.bbc_event_registrations
-  DROP COLUMN IF EXISTS phone_country_code;
+  CHECK (phone ~ '^\\+[1-9][0-9]{7,14}$');

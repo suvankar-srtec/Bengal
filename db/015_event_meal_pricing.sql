72b971a459e18609159c6f5079950b0930455b6a
suvankar-srtec <suvankar.srtec@gmail.com>
2026-09-24 19:03:27 +0530

    Apply meal charge to registration total constraint

--- db/015_event_meal_pricing.sql
+++ db/015_event_meal_pricing.sql
@@ -36,3 +36,16 @@ ALTER TABLE public.bbc_meal_redemptions
 ALTER TABLE public.bbc_meal_redemptions
   ADD CONSTRAINT bbc_meal_redemptions_meal_choice_check
     CHECK (meal_choice IN ('snacks', 'lunch', 'dinner'));
+
+
+ALTER TABLE public.bbc_event_registrations
+  DROP CONSTRAINT IF EXISTS bbc_event_registrations_total_paise_check;
+
+ALTER TABLE public.bbc_event_registrations
+  ADD CONSTRAINT bbc_event_registrations_total_paise_check
+  CHECK (
+    total_paise = participation_quantity * participation_unit_paise
+      + standee_quantity * standee_unit_paise
+      + CASE WHEN meal_choice IS NOT NULL THEN participation_quantity * meal_unit_paise ELSE 0 END
+      + CASE WHEN presentation_selected THEN presentation_unit_paise ELSE 0 END
+  );

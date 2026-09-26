30543a27b81a4d26d92a7137438afcd44f485dad
suvankar-srtec <suvankar.srtec@gmail.com>
2026-09-26 19:21:14 +0530

    Store event start and end times

--- db/023_event_time.sql
+++ db/023_event_time.sql
@@ -1,2 +1,5 @@
 ALTER TABLE public.bbc_event_content
   ADD COLUMN IF NOT EXISTS event_time TIME NOT NULL DEFAULT TIME '18:00';
+
+ALTER TABLE public.bbc_event_content
+  ADD COLUMN IF NOT EXISTS event_end_time TIME NOT NULL DEFAULT TIME '20:00';
